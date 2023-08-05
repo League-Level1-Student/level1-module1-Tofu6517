12e36d278011248fc8144444e019c89f34aa1298
jereycui2008 <jeremycuid@gmail.com>
2023-08-05 16:26:14 -0700

    a

---
+++ src/_03_cat/CatRunner.java
@@ -0,0 +1,13 @@
+package _03_cat;
+
+public class CatRunner {
+	public static void main(String[] args) {
+	
+	Cat luna= new Cat("luna");
+	
+	luna.meow();
+	luna.printName();
+	luna.kill();
+	
+}
+}
