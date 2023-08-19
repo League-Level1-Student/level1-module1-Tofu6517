1e41144e173a9718de5c478562be2af326ab199c
jereycui2008 <jeremycuid@gmail.com>
2023-08-19 16:29:13 -0700

    a

--- src/_06_vault/VaultRunner.java
+++ src/_06_vault/VaultRunner.java
@@ -2,23 +2,26 @@ package _06_vault;
 
 public class VaultRunner {
 	public static void main(String[] args) {
-		
 		Vault vault=new Vault();
-		vault.tryCode(guess);
-		
+		SecretAgent secretAgent=new SecretAgent();
+		int i=secretAgent.findCode(vault);
+		boolean correct=vault.tryCode(i);
+		System.out.println(correct);
 		
 		
 	}
 }
-	class SecretAgent{
-	int findCode(Vault guess) {
-		for(int i=0;i<1000001;i++ ) {
-			return i;
+class SecretAgent {
+	int findCode(Vault v) {
+		for(int i=0;i<1000000001;i++ ) {
+			boolean correct=v.tryCode(i);
+			if(correct) {
+				return i;
+			}
 		}
-
+		return -1;
 	}
-		
-	}
+}
 	
 	
 	
